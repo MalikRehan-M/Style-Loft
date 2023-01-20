ceac56ca525da9a4cf3691ed21b50bcec983c24b
MalikRehan-M <malikrehan.meeranaik@gmail.com>
2023-01-20 15:34:37 +0530

    day_3

--- style/src/Components/Carousel.jsx
+++ style/src/Components/Carousel.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
+import SimpleImageSlider from "react-simple-image-slider";
+const images = [
+  { url:"https://assets0.mirraw.com/banner_sliders/10031/05_%281%29_main_webp.webp?1674107278"},
+  { url: "https://assets0.mirraw.com/banner_sliders/10027/01_%281%29_main_webp.webp?1674107117" },
+  { url: "https://assets0.mirraw.com/banner_sliders/10028/02_%282%29_main_webp.webp?1674107156" },
+  { url: "https://assets0.mirraw.com/banner_sliders/10029/04_%281%29_main_webp.webp?1674107206" },
+  { url: "https://assets0.mirraw.com/banner_sliders/9685/08_main_webp.webp?1672649895" }
+];
 
 const Carousel = () => {
   return (
-    <div>
-      Carousel
+    <div style={{width:"100%",backgroundSize:"contain"}}>
+      <SimpleImageSlider
+        width="100%"
+        height={704}
+        images={images}
+        // showBullets={true}
+        showNavs={true}
+        autoPlay={true}
+        autoPlayDelay={3.5}
+      />
     </div>
-  )
+  );
 }
-
 export default Carousel
 
 // const slides = [
---
+++ style/src/Components/Midsection.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { Flex, Image } from "@chakra-ui/react";
+
+const Midsection = () => {
+  return (
+    <Flex w="65%" m="1% auto">
+      {/* first column */}
+      <Flex m="0" p="0" direction="column" >
+        <Image src="https://assets0.mirraw.com/frontpages/13065/B_01_original_sized_webp.webp?1673848746" />
+        <Flex m="0" p="0" direction="column">
+          <Image src="https://assets0.mirraw.com/frontpages/13072/B_07_original_sized_webp.webp?1673848943" />
+          <Image src="https://assets0.mirraw.com/frontpages/13074/B_10_original_sized_webp.webp?1673849000" />
+          <Image src="https://assets0.mirraw.com/frontpages/13081/B_16_original_sized_webp.webp?1673849217" />
+          <Image src="https://assets0.mirraw.com/frontpages/13083/B_19_original_sized_webp.webp?1673849277" />
+          <Image src="https://assets0.mirraw.com/frontpages/13090/B_25_original_sized_webp.webp?1673849496" />
+          <Image src="https://assets0.mirraw.com/frontpages/13092/B_28_original_sized_webp.webp?1673849686" />
+          <Image src="https://assets0.mirraw.com/frontpages/13097/B_34_original_sized_webp.webp?1673849841" />
+          <Image src="https://assets0.mirraw.com/frontpages/13101/B_37_original_sized_webp.webp?1673849953" />
+          <Image src="https://assets0.mirraw.com/frontpages/13107/B_43_original_sized_webp.webp?1673850158" />
+        </Flex>
+      </Flex>
+      {/* second col */}
+      <Flex m="0" p="0" direction="column" >
+        <Image src="https://assets0.mirraw.com/frontpages/13066/B_02_original_sized_webp.webp?1673848773" />
+        <Image src="https://assets0.mirraw.com/frontpages/13067/B_04_original_sized_webp.webp?1673848798" />
+        <Image src="https://assets0.mirraw.com/frontpages/13073/B_08_original_sized_webp.webp?1673848956" />
+        <Image src="https://assets0.mirraw.com/frontpages/13076/B_11_original_sized_webp.webp?1673849050" />
+        <Image src="https://assets0.mirraw.com/frontpages/13075/B_13_original_sized_webp.webp?1673849037" />
+        <Image src="https://assets0.mirraw.com/frontpages/13082/B_17_original_sized_webp.webp?1673849236" />
+        <Image src="https://assets0.mirraw.com/frontpages/13084/B_20_original_sized_webp.webp?1673849319" />
+        <Image src="https://assets0.mirraw.com/frontpages/13085/B_22_original_sized_webp.webp?1673849332" />
+        <Image src="https://assets0.mirraw.com/frontpages/13091/B_26_original_sized_webp.webp?1673849512" />
+        <Image src="https://assets0.mirraw.com/frontpages/13093/B_29_original_sized_webp.webp?1673849717" />
+        <Image src="https://assets0.mirraw.com/frontpages/13094/B_31_original_sized_webp.webp?1673849738" />
+        <Image src="https://assets0.mirraw.com/frontpages/13098/B_35_original_sized_webp.webp?1673849864" />
+        <Image src="https://assets0.mirraw.com/frontpages/13102/B_38_original_sized_webp.webp?1673849987" />
+        <Image src="https://assets0.mirraw.com/frontpages/13104/B_40_original_sized_webp.webp?1673850064" />
+        <Image src="https://assets0.mirraw.com/frontpages/13108/B_44_original_sized_webp.webp?1673850175" />
+      </Flex>
+      {/* third col */}
+      <Flex m="0" p="0" direction="column">
+        <Image src="https://assets0.mirraw.com/frontpages/13068/B_03_original_sized_webp.webp?1673848820" />
+        <Flex>
+          <Flex m="0" p="0" direction="column" >
+            <Image src="https://assets0.mirraw.com/frontpages/13069/B_05_original_sized_webp.webp?1673848853" />
+            <Image src="https://assets0.mirraw.com/frontpages/13070/B_09_original_sized_webp.webp?1673848869" />
+          </Flex>
+          <Flex m="0" p="0" direction="column">
+            <Image src="https://assets0.mirraw.com/frontpages/13071/B_06_original_sized_webp.webp?1673848907" />
+          </Flex>
+        </Flex>
+        <Image src="https://assets0.mirraw.com/frontpages/13077/B_12_original_sized_webp.webp?1673849087" />
+        <Flex>
+          <Flex m="0" p="0" direction="column">
+            <Image src="https://assets0.mirraw.com/frontpages/13078/B_14_original_sized_webp.webp?1673849138" />
+            <Image src="https://assets0.mirraw.com/frontpages/13079/B_18_original_sized_webp.webp?1673849157" />
+          </Flex>
+          <Flex m="0" p="0" direction="column">
+            <Image src="https://assets0.mirraw.com/frontpages/13080/B_15_original_sized_webp.webp?1673849190" />
+          </Flex>
+        </Flex>
+        <Image src="https://assets0.mirraw.com/frontpages/13086/B_21_original_sized_webp.webp?1673849357" />
+        <Flex>
+          <Flex m="0" p="0" direction="column">
+            <Image src="https://assets0.mirraw.com/frontpages/13088/B_23_original_sized_webp.webp?1673849417" />
+            <Image src="https://assets0.mirraw.com/frontpages/13087/B_27_original_sized_webp.webp?1673849402" />
+          </Flex>
+          <Flex m="0" p="0" direction="column">
+            <Image src="https://assets0.mirraw.com/frontpages/13089/B_24_original_sized_webp.webp?1673849458" />
+          </Flex>
+        </Flex>
+        <Image src="https://assets0.mirraw.com/frontpages/13096/B_30_original_sized_webp.webp?1673849793" />
+        <Flex>
+          <Flex m="0" p="0" direction="column">
+            <Image src="https://assets0.mirraw.com/frontpages/13099/B_32_original_sized_webp.webp?1673849905" />
+            <Image src="https://assets0.mirraw.com/frontpages/13100/B_36_original_sized_webp.webp?1673849916" />
+          </Flex>
+          <Flex m="0" p="0" direction="column">
+            <Image src="https://assets0.mirraw.com/frontpages/13095/B_33_original_sized_webp.webp?1673849765" />
+          </Flex>
+        </Flex>
+        <Image src="https://assets0.mirraw.com/frontpages/13103/B_39_original_sized_webp.webp?1673850003" />
+        <Flex>
+          <Flex m="0" p="0" direction="column">
+            <Image src="https://assets0.mirraw.com/frontpages/13105/B_41_original_sized_webp.webp?1673850083" />
+            <Image src="https://assets0.mirraw.com/frontpages/13109/B_45_original_sized_webp.webp?1673850200" />
+          </Flex>
+          <Flex m="0" p="0" direction="column">
+            <Image src="https://assets0.mirraw.com/frontpages/13106/B_42_original_sized_webp.webp?1673850129" />
+          </Flex>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Midsection;
